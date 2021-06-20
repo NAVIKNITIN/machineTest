69da4dc22421979d54d7231196f57d38dddd8029
NAVIK <naviknitin143@gmail.com>
2021-06-20 09:13:37 +0530

    register

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import NavBar from "./component/Navbar";
 import PrivateRoute from "./component/PrivateRoute";
 import UserContext from "./contextstorage/userContext";
 import Login from "./component/Login";
-import SignUp from "./component/Register";
+import Register from "./component/Register";
 import Task from "./component/task";
 import User from "./component/user";
 // 
@@ -24,7 +24,7 @@ function App() {
 						<PrivateRoute path="/tasks" component={Task} />
 						<PrivateRoute path="/user" component={User} />
 						<Route path="/login" component={Login} />
-						<Route path="/signup" component={SignUp} /> 
+						<Route path="/register" component={Register} /> 
 					</Switch>
 				</Container>
 			</Router>
