a9e175097b6e1bc0b759d61058cf9b786b62da32
NAVIK <naviknitin143@gmail.com>
2021-06-21 09:14:37 +0530

    info

--- src/component/info.jsx
+++ src/component/info.jsx
@@ -4,8 +4,5 @@ const Info = ({ text }) => {
   return <p> {text} </p>;
 };
 
-Info.defaultProps = {
-  text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus voluptatum vel perspiciatis officia ducimus nemo autem ex! Expedita amet aut, fugit quaerat eligendi nisi corrupti, eaque obcaecati, adipisci dicta aliquid?",
-};
 
 export default Info;
